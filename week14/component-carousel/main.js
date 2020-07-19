0fb2882b48f67d222f00325a0c3dfa63aad5021c
likang <lkang@baseplate.co.nz>
2020-07-19 20:39:35 +1200

    使用animation类代替js手动的更改element动画

--- week14/component-carousel/main.js
+++ week14/component-carousel/main.js
@@ -1,4 +1,5 @@
 import {create,Text,Wrapper} from './createElement'
+import { Animation,Timeline } from '../../week15/animation/animation'
 
 class Carousel{
     constructor(){
@@ -33,87 +34,95 @@ class Carousel{
             let next = children[nextPosition]
            
             //动画的起始位置不能有transition
-            current.style.transition = "ease 0s"
-            next.style.transition = "ease 0s"
+            // current.style.transition = "ease 0s"
+            // next.style.transition = "ease 0s"
             
             //动画的起始位置
-            current.style.transform = `translateX(${ -100 * position}%)`
-            next.style.transform = `translateX(${100 -100 * nextPosition}%)`
+            // current.style.transform = `translateX(${ -100 * position}%)`
+            // next.style.transform = `translateX(${100 -100 * nextPosition}%)`
 
             setTimeout(() => {//transition生效是需要一定的时间的，所以需要settimeout，不能顺次的写
-                current.style.transition ="" //means use css rules ease 0.5s
-               next.style.transition = ""
+                // current.style.transition ="" //means use css rules ease 0.5s
+                // next.style.transition = ""
+                
+                //使用Animation类代替之前的js手写动画
+                let timeLine = new Timeline()
+                let currentAnimation = new Animation(current.style,'transform',-100 * position,-100 -100 * position,500,0,t=>t,v=>`translateX(${v}%)`)
+                let nextAnimation = new Animation(next.style,'transform',100 -100 * nextPosition,-100  * nextPosition,500,0,t=>t,v=>`translateX(${v}%)`)
+                timeLine.add(currentAnimation)
+                timeLine.add(nextAnimation)
+                timeLine.start()
+
                 //动画的终止位置
-                console.log('end:',`translateX(${-100 -100 * position}%)`,`translateX(${-100 * nextPosition}%)`)
-                current.style.transform = `translateX(${-100 -100 * position}%)`
-                next.style.transform = `translateX(${-100 * nextPosition}%)`
+                // current.style.transform = `translateX(${-100 -100 * position}%)`
+                // next.style.transform = `translateX(${-100 * nextPosition}%)`
 
                 position = nextPosition
-                console.log('position',position)
             }, 16);
-
             setTimeout(nextPic,3000)
         }
-       // setTimeout(nextPic,3000)
+        setTimeout(nextPic,3000)
+        
+
        //鼠标拖拽
-       root.addEventListener('mousedown',(e)=>{
-            let startX = e.clientX;
-            let nextPosition = (position + 1) % this.data.length
-            let lastPosition = (position -1 + this.data.length) % this.data.length //不能直接position -1, 有可能出现负数，因此要加上data.length
+    //    root.addEventListener('mousedown',(e)=>{
+    //         let startX = e.clientX;
+    //         let nextPosition = (position + 1) % this.data.length
+    //         let lastPosition = (position -1 + this.data.length) % this.data.length //不能直接position -1, 有可能出现负数，因此要加上data.length
             
-            let current = children[position]
-            let next = children[nextPosition]
-            let last = children[lastPosition]
+    //         let current = children[position]
+    //         let next = children[nextPosition]
+    //         let last = children[lastPosition]
 
-            //初始化transition
-            current.style.transition = "ease 0s"
-            next.style.transition = "ease 0s"
-            last.style.transition = "ease 0s"
+    //         //初始化transition
+    //         current.style.transition = "ease 0s"
+    //         next.style.transition = "ease 0s"
+    //         last.style.transition = "ease 0s"
 
-            //调整到正确的位置
-            //应该用csssom去取以下它们的尺寸
-            current.style.transform = `translateX(${ -500 * position}px)`
-            last.style.transform = `translateX(${-500 -500 * lastPosition}px)`
-            next.style.transform = `translateX(${500 -500 * nextPosition}px)`
+    //         //调整到正确的位置
+    //         //应该用csssom去取以下它们的尺寸
+    //         current.style.transform = `translateX(${ -500 * position}px)`
+    //         last.style.transform = `translateX(${-500 -500 * lastPosition}px)`
+    //         next.style.transform = `translateX(${500 -500 * nextPosition}px)`
             
-            let move = event=>{
-                //500px的container
-                current.style.transform = `translateX(${event.clientX - startX -500 * position}px)`
-                last.style.transform = `translateX(${event.clientX - startX -500 - 500 * lastPosition}px)`
-                next.style.transform = `translateX(${event.clientX - startX + 500 -500  * nextPosition}px)`
+    //         let move = event=>{
+    //             //500px的container
+    //             current.style.transform = `translateX(${event.clientX - startX -500 * position}px)`
+    //             last.style.transform = `translateX(${event.clientX - startX -500 - 500 * lastPosition}px)`
+    //             next.style.transform = `translateX(${event.clientX - startX + 500 -500  * nextPosition}px)`
 
-            }
-            let up = event=>{//document监听即使离开了浏览器范围也还是会继续监听的
-                //拖过一半500/2就认为可以变下一张了
-                let offset = 0
-                if(event.clientX - startX > 250){
-                    offset = 1
-                }else if(event.clientX - startX <- 250){
-                    offset = -1
-                }
+    //         }
+    //         let up = event=>{//document监听即使离开了浏览器范围也还是会继续监听的
+    //             //拖过一半500/2就认为可以变下一张了
+    //             let offset = 0
+    //             if(event.clientX - startX > 250){
+    //                 offset = 1
+    //             }else if(event.clientX - startX <- 250){
+    //                 offset = -1
+    //             }
 
-                //加transition打开
-                current.style.transition = ""
-                next.style.transition = ""
-                last.style.transition = ""
+    //             //加transition打开
+    //             current.style.transition = ""
+    //             next.style.transition = ""
+    //             last.style.transition = ""
 
-                //带动画的transform：和鼠标没有关系了
-                current.style.transform = `translateX(${offset*500 -500 * position}px)`
-                last.style.transform = `translateX(${offset*500 -500 - 500 * lastPosition}px)`
-                next.style.transform = `translateX(${offset*500 + 500 -500  * nextPosition}px)`
+    //             //带动画的transform：和鼠标没有关系了
+    //             current.style.transform = `translateX(${offset*500 -500 * position}px)`
+    //             last.style.transform = `translateX(${offset*500 -500 - 500 * lastPosition}px)`
+    //             next.style.transform = `translateX(${offset*500 + 500 -500  * nextPosition}px)`
 
-                //用取余运算来实现循环的下标
-                position = (position - offset + this.data.length) % this.data.length
+    //             //用取余运算来实现循环的下标
+    //             position = (position - offset + this.data.length) % this.data.length
                 
-                //remove event
-                document.removeEventListener('mousemove',move)
-                document.removeEventListener('mouseup',up)
-            }
-            document.addEventListener('mousemove',move)
-            document.addEventListener('mouseup',up)
-        })
+    //             //remove event
+    //             document.removeEventListener('mousemove',move)
+    //             document.removeEventListener('mouseup',up)
+    //         }
+    //         document.addEventListener('mousemove',move)
+    //         document.addEventListener('mouseup',up)
+    //     })
         return root
-    }
+     }
 }
 
 
